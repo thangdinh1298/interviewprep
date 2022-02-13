3740b3c00ca81e8f858441c2016f608d96b25d26
thangdinh1298 <thangdinh.umbala@gmail.com>
2022-02-13 22:33:11 +0700

    Added quicksort

--- algorithms/sort/sort.cpp
+++ algorithms/sort/sort.cpp
@@ -2,6 +2,30 @@
 #include <vector>
 
 /*
+ * Utility functions ==================================
+ */
+
+void print(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void print(const std::vector<int>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void swap(int& a, int& b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+/*
  * Selection sort =======================================
  */
 void selection_sort(int arr[], int n) {
@@ -72,21 +96,27 @@ void merge_sort(std::vector<int>& v, int lo, int hi) {
 }
 
 /*
- * Utility functions ==================================
+ * Quick sort =============================================
  */
-
-void print(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
+int partition(int arr[], int l, int r) {
+    int pivot_index = r; //Choose last element as pivot
+    int i = l - 1;
+    for (int j = l; j < r; j++) {
+        if (arr[j] <= arr[pivot_index]) {
+            i++; //Increase boundary for left partition by one
+            swap(arr[i], arr[j]); //Put element at j into the left partition
+        }
     }
-    std::cout << std::endl;
+    swap(arr[i+1], arr[pivot_index]); //Swap pivot element with first element in right partition
+    return i + 1;
 }
 
-void print(const std::vector<int>& v) {
-    for (int i = 0; i < v.size(); i++) {
-        std::cout << v[i] << " ";
+void quicksort(int arr[], int l, int r) {
+    if (l < r) { //Need to check like this because we will be calling quicksort with l..p-1 and p+1..r
+        int p = partition(arr, l, r);
+        quicksort(arr, l, p - 1);
+        quicksort(arr, p + 1, r);
     }
-    std::cout << std::endl;
 }
 
 /*
@@ -94,10 +124,12 @@ void print(const std::vector<int>& v) {
  */
 
 int main() {
+    std::cout << "=============================Selection sort=================================\n";
     int arr1[] = {1,3,2,5,1};
     selection_sort(arr1, 5);
     print(arr1, 5);
 
+    std::cout << "=============================Merge sort=================================\n";
     std::vector<int> v1 = {1,3,2,5,1};
     std::vector<int> v2 = {1,1,1,1,1};
     std::vector<int> v3 = {1};
@@ -116,4 +148,24 @@ int main() {
     print(v4);
     print(v5);
     print(v6);
+
+    std::cout << "=============================Quick sort=================================\n";
+    int a1[] = {1,3,2,5,1};
+    int a2[] = {1,1,1,1,1};
+    int a3[] = {1};
+    int a4[] = {15,1};
+    int a5[] = {1,2,3,4,5};
+    int a6[] = {5,4,3,2,1};
+    quicksort(a1, 0, 4);
+    quicksort(a2, 0, 4);
+    quicksort(a3, 0, 0);
+    quicksort(a4, 0, 1);
+    quicksort(a5, 0, 4);
+    quicksort(a6, 0, 4);
+    print(a1, 5);
+    print(a2, 5);
+    print(a3, 1);
+    print(a4, 2);
+    print(a5, 5);
+    print(a6, 5);
 }
